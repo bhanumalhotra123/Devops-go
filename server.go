6ba9ffdbeea8a6a261c5ec419b75ee294af29819
bhanumalhotra <bhanumalhotra121@gmail.com>
2023-09-18 17:17:31 +0530

    update

--- server.go
+++ server.go
@@ -38,7 +38,6 @@ func main() {
 
 // The "handleHelloWorld" function handles requests to the "/hello-world" endpoint.
 func handleHelloWorld(writer http.ResponseWriter, request *http.Request) {
-	// This function is currently empty and doesn't do anything.
 	// It's a placeholder for handling the "/hello-world" endpoint.
 	if request.Method != "GET" {
 		http.Error(writer, http.StatusText(http.StatusMethodNotAllowed), http.StatusMethodNotAllowed)
@@ -50,7 +49,6 @@ func handleHelloWorld(writer http.ResponseWriter, request *http.Request) {
 
 // The "handleHealth" function handles requests to the "/health" endpoint.
 func handleHealth(writer http.ResponseWriter, request *http.Request) {
-	// This function is also currently empty and doesn't do anything.
 	// It's a placeholder for handling the "/health" endpoint.
 	if request.Method != "GET" {
 		http.Error(writer, http.StatusText(http.StatusMethodNotAllowed), http.StatusMethodNotAllowed)
